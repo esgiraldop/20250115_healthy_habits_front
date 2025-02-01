76165686c9a8d5bd70ce925cb9ae887d6b8225a6
Erick Giraldo <erickgiraldo94@gmail.com>
2025-02-01 17:03:00 -0500

    chore: added additional configs to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,4 +49,12 @@ module.exports = {
       },
     },
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {},
+    },
+  ],
 };
